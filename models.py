5107e67b1e7cb551e3fd7a42f17b3a56a72e2eae
jliechty <jliechty@f04a3889-0f81-4131-97fb-bc517d1f583d>
2014-02-07 18:13:52 +0000

    checkin prior to dev site checkout
    git-svn-id: svn+ssh://digitalprojects.library.unt.edu/var/lib/svn/trac/django_apps/editor/invite@6850 f04a3889-0f81-4131-97fb-bc517d1f583d

--- models.py
+++ models.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.models import User, Permission, Group
 from django.template.loader import render_to_string
+from django.core.files.base import ContentFile
 from django.contrib.sites.models import Site
 from django.core.mail import send_mail
+from django.core.files import File
 from django.db import models
 import settings
 import uuid
+from cStringIO import StringIO
 
 
 class InviteItem(models.Model):
