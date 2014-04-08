79ad205f74f63616f98380b20481f29ba52ef590
digisvn <digisvn@f04a3889-0f81-4131-97fb-bc517d1f583d>
2014-04-08 18:24:55 +0000

    jliechty: guess i forgot to ever add the tests module
    git-svn-id: svn+ssh://digitalprojects.library.unt.edu/var/lib/svn/trac/django_apps/editor/invite@7059 f04a3889-0f81-4131-97fb-bc517d1f583d

---
+++ tests.py
@@ -0,0 +1,291 @@
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium import webdriver
+from django.conf import settings
+from django.contrib.sites.models import Site
+from django.template.loader import render_to_string
+from django.test.client import Client
+from django.contrib.auth.models import User
+
+import unittest
+import time
+import requests
+try: import simplejson as json
+except ImportError: import json
+import settings
+import mock
+from .models import Invitation, PasswordResetInvitation
+
+
+class TestOperations(unittest.TestCase):
+
+    def test_invite_creation(self):
+        '''creates an invite and check to see if we can send the email'''
+        i = Invitation.objects.create(
+            email='joeyliechty@supergreatmail.com',
+        )
+        i.save()
+        self.assertTrue(Invitation.objects.count() > 0)
+
+    @mock.patch('invite.models.send_mail')
+    def test_email_send(self, mock_django_mailer):
+        '''
+        since we trust the django mailer, and we don't want to do time wasting
+        tests that do actual network activity, we'll use mock to ensure that
+        the mailer is called with the correct arguments by the model
+        '''
+        i = Invitation.objects.create(
+            email='1234testemail@noone.ghost',
+            username='test',
+            first_name='test',
+            last_name='test',
+        )
+        message = render_to_string(
+            'invite/invitation_email.txt',
+            {
+                'domain': Site.objects.get_current().domain,
+                'service_name': settings.SERVICE_NAME,
+                'activation_code': i.activation_code,
+                'custom_msg': i.custom_msg,
+            }
+        )
+        i.send()
+        mock_django_mailer.assert_called_with(
+            'You have been invited to join the %s' % (settings.SERVICE_NAME),
+            message,
+            settings.DEFAULT_FROM_EMAIL,
+            [i.email],
+        )
+
+    def test_password_reset_invite_creation(self):
+        '''creates an invite and check to see if we can send the email'''
+        i = PasswordResetInvitation.objects.create(
+            email='joeyliechty@supergreatmail.com',
+        )
+        i.save()
+        self.assertTrue(Invitation.objects.count() > 0)
+
+    @mock.patch('invite.models.send_mail')
+    def test_password_reset_email_send(self, mock_django_mailer):
+        '''
+        since we trust the django mailer, and we don't want to do time wasting
+        tests that do actual network activity, we'll use mock to ensure that
+        the mailer is called with the correct arguments by the model
+        '''
+        i = PasswordResetInvitation.objects.create(
+            email='1234testemail@noone.ghost',
+            username='test',
+            first_name='test',
+            last_name='test',
+        )
+        subject = 'Password Reset: %s' % (settings.SERVICE_NAME)
+        message = render_to_string(
+            'invite/reset_email.txt',
+            {
+                'first_name': i.first_name,
+                'username': i.username,
+                'domain': Site.objects.get_current().domain,
+                'reset_code': i.activation_code,
+            }
+        )
+        i.send()
+        mock_django_mailer.assert_called_with(
+            subject,
+            message,
+            settings.DEFAULT_FROM_EMAIL,
+            [i.email],
+        )
+
+
+class TestViews(unittest.TestCase):
+    def setUp(self):
+        self.c = Client()
+        self.user = User.objects.get_or_create(
+            username='kingkong',
+            email='test@test.test',
+            password='test',
+        ) 
+        self.i = Invitation.objects.create(
+            username='asdf',
+            first_name='test',
+            last_name='test',
+            email='test@test.test'
+        ) 
+        self.psi = PasswordResetInvitation.objects.create(
+            email='1234testemail@noone.ghost',
+            username='kingkong',
+            first_name='test',
+            last_name='test',
+        )
+
+    def test_amnesia_email_submit(self):
+        response = self.c.post('/accounts/amnesia/', {'email': 'avowin@test.test'})
+        self.assertIn('Email doesnt belong to any user', response.content)
+
+    def test_signup_submit_same_email(self):
+        response = self.c.post(
+            '/accounts/signup/?code=%s' % self.i.activation_code,
+            {
+                'username': 'don',
+                'first_name': 'wrigley',
+                'last_name': 'piggly',
+                'email': 'test@test.test',
+                'password': 'test',
+                'password2': 'test',
+            }
+        )
+        self.assertIn('Email exists on other user', response.content)
+
+    def test_invite_submit_same_email(self):
+        my_admin = User.objects.create_superuser('test', 'myemail@test.com', 'test')
+        self.c.login(username='test', password='test')
+        response = self.c.post(
+            '/accounts/invite/',
+            {
+                u'form-MAX_NUM_FORMS': [u''],
+                u'form-0-email': [u'test@test.test'],
+                u'form-TOTAL_FORMS': [u'1'],
+                u'form-0-username': [u'test'],
+                u'form-INITIAL_FORMS': [u'0'],
+                u'form-0-last_name': [u'test'],
+                u'form-0-first_name': [u'test'],
+                u'form-0-greeting': [u'']
+            },
+        )
+        self.assertIn('Email exists on other user', response.content)
+
+    def test_reset_submit(self):
+        user = User.objects.create(
+            username='django',
+            email='1234testemail@noone.ghost',
+            password='test',
+            first_name='test',
+            last_name='test',
+        )
+        psi = PasswordResetInvitation.objects.create(
+            email='1234testemail@noone.ghost',
+            username='django',
+            first_name='test',
+            last_name='test',
+        )
+        response = self.c.post('/accounts/reset/', {'password': 'test', 'password2': 'pest'})
+        self.assertIn('Passwords are not the same', response.content)
+        response = self.c.post(
+            '/accounts/reset/?reset_code=%s' % psi.activation_code,
+            {'password': 'test', 'password2': 'test'},
+            follow=True
+        )
+        self.assertIn('The UNT Digital Library: Dashboard', response.content)
+
+
+class FunctionalTestCase(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser2 = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+        self.browser2.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+        self.browser2.quit()
+
+
+class TestFunctional(FunctionalTestCase):
+    def test_one_basic_invite(self):
+        # joey opens the browser and goes to the  url
+        self.browser.get('http://129.120.93.131:8000/accounts/')
+        self.browser.find_element_by_name('username').send_keys('test')
+        self.browser.find_element_by_name('password').send_keys('test')
+        self.browser.find_element_by_xpath('//*[@id="body"]/div[2]/div/form/input').click()
+        self.browser.find_element_by_xpath('//*[@id="body"]/div[2]/div/div/a').click()
+        # type in our username and password and send the return key
+        self.browser.find_element_by_name('form-0-first_name').send_keys('jayjay')
+        self.browser.find_element_by_name('form-0-last_name').send_keys('jetplane')
+        self.browser.find_element_by_name('form-0-username').send_keys('jetplane')
+        self.browser.find_element_by_name('form-0-email').send_keys('asdf@asdf.asdf')
+        # click send
+        self.browser.find_element_by_xpath('//*[@id="body"]/div[2]/div/form/div[3]/div[4]/div[2]/input').click()
+        body = self.browser.find_element_by_tag_name('body')
+        time.sleep(1)
+        self.assertIn('Jayjay', body.text)
+        self.browser.find_element_by_xpath('//*[@id="body"]/div[2]/div/div/div[2]/table/tbody/tr[2]/td[3]/form/input').click()
+
+    def test_one_complex_invite_and_registration(self):
+        # grab a trash email address
+        self.browser2.get('http://mailinator.com/')
+        email = self.browser2.find_element_by_id('inboxfield').send_keys('jayjayjetplane@mailinator.com')
+        self.browser2.find_element_by_xpath('/html/body/div[2]/div/div[1]/div[1]/div/div[2]/div/div/btn').click()
+        # joey opens the browser and goes to the  url
+        self.browser.get('http://129.120.93.131:8000/accounts/')
+        self.browser.find_element_by_name('username').send_keys('test')
+        self.browser.find_element_by_name('password').send_keys('test')
+        self.browser.find_element_by_xpath('//*[@id="body"]/div[2]/div/form/input').click()
+        # click the invite button
+        self.browser.find_element_by_xpath('//*[@id="body"]/div[2]/div/div/a').click()
+        # type in our username and password and send the return key
+        self.browser.find_element_by_name('form-0-first_name').send_keys('jayjay')
+        self.browser.find_element_by_name('form-0-last_name').send_keys('jetplane')
+        self.browser.find_element_by_name('form-0-username').send_keys('jetplane')
+        self.browser.find_element_by_name('form-0-email').send_keys('jayjayjetplane@mailinator.com')
+        # make superuser
+        self.browser.find_element_by_xpath('//*[@id="id_form-0-is_super_user"]').click()
+        self.browser.find_element_by_name('form-0-greeting').send_keys('This is a custom greeting that will be send to all email addresses.')
+        # select the permissions and groups
+        select = Select(self.browser.find_element_by_id('id_form-0-permissions'))
+        select.select_by_visible_text('invite | invitation | Can add invitation')
+        groups = Select(self.browser.find_element_by_id('id_form-0-groups'))
+        groups.select_by_visible_text('ALL INVITE')
+        # click send
+        self.browser.find_element_by_xpath('//*[@id="body"]/div[2]/div/form/div[3]/div[4]/div[2]/input').click()
+        body = self.browser.find_element_by_tag_name('body')
+        time.sleep(1)
+        self.assertIn('Jayjay', body.text)
+        # give a bit of time for the email to arrive.
+        time.sleep(15)
+        self.browser2.get('http://mailinator.com/inbox.jsp?to=jayjayjetplane')
+        self.browser2.find_element_by_xpath('//*[@id="mailcontainer"]/li[1]/a/div[1]').click()
+        self.browser2.find_element_by_xpath('//*[@id="mailshowdiv"]/div[3]/div/div/a').click()
+        window_handles = self.browser2.window_handles
+        self.browser2.switch_to_window(window_handles[1])
+        self.browser2.find_element_by_xpath('//*[@id="id_password"]').send_keys('password')
+        self.browser2.find_element_by_xpath('//*[@id="id_password2"]').send_keys('password')
+        self.browser2.find_element_by_xpath('//*[@id="body"]/div[2]/div/div/form/div[5]/input').click()
+
+    def test_eleven_basic_invites(self):
+        # joey opens the browser and goes to the  url
+        self.browser.get('http://129.120.93.131:8000/accounts/')
+        self.browser.find_element_by_name('username').send_keys('test')
+        self.browser.find_element_by_name('password').send_keys('test')
+        self.browser.find_element_by_xpath('//*[@id="body"]/div[2]/div/form/input').click()
+        self.browser.find_element_by_xpath('//*[@id="body"]/div[2]/div/div/a').click()
+        for i in range(10):
+            self.browser.find_element_by_xpath('//*[@id="add"]/i').click()
+        for i in range(11):
+            # type in data
+            self.browser.find_element_by_name('form-%s-first_name' % i).send_keys('jayjay')
+            self.browser.find_element_by_name('form-%s-last_name' % i).send_keys('jetplane')
+            self.browser.find_element_by_name('form-%s-username' % i).send_keys('jetplane')
+            self.browser.find_element_by_name('form-%s-email' % i).send_keys('asdf@asdf.asdf')
+        # click send
+        self.browser.find_element_by_xpath('//*[@id="body"]/div[2]/div/form/div[3]/div[4]/div[2]/input').click()
+        body = self.browser.find_element_by_tag_name('body')
+        time.sleep(1)
+        self.assertIn('Jayjay', body.text)
+        # delete the invites
+        for i in range(11):
+            self.browser.find_element_by_xpath('//*[@id="body"]/div[2]/div/div/div[2]/table/tbody/tr[2]/td[3]/form/input').click()
+
+    def test_forgotten_password(self):
+        '''user forgets his password test'''
+        s = Site.objects.get_current()
+        PasswordResetInvitation.objects.create()
+        self.browser.get('http://%s/accounts/signin/' % s.domain)
+        self.browser.find_element_by_xpath('//*[@id="body"]/div[2]/div/form/a').click()
+        self.browser.find_element_by_xpath('//*[@id="id_email"]').send_keys('smellycakes@mailinator.com')
+        self.browser.find_element_by_xpath('//*[@id="body"]/div[2]/div/div/div/form/input[2]').click()
+        time.sleep(5)
+        email = requests.get('https://api.mailinator.com/api/inbox?token=6346fe2574a246eab17f7e1ab75ce993')
+        print email
+
+if __name__ == '__main__':
+    unittest.main()
