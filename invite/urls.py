0c75b9006319d532e4c9a29bd498bfaef9928656
Gio Gottardi <somexpert@gmail.com>
2017-02-09 10:27:47 -0600

    Update URL confs to stop using deprecated patterns().

--- invite/urls.py
+++ invite/urls.py
@@ -1,15 +1,16 @@
-from django.conf.urls import url, patterns
+from django.conf.urls import url
 
-urlpatterns = patterns(
-    '',
-    url(r'^$', 'invite.views.index', name='index'),
-    url(r'^invite/$', 'invite.views.invite', name='invite'),
-    url(r'^resend/(?P<code>.*)/$', 'invite.views.resend', name='resend'),
-    url(r'^revoke/(?P<code>.*)/$', 'invite.views.revoke', name='revoke'),
-    url(r'^login/$', 'invite.views.log_in_user', name='login'),
-    url(r'^logout/$', 'invite.views.log_out_user', name='edit_logout'),
-    url(r'^amnesia/$', 'invite.views.amnesia', name='amnesia'),
-    url(r'^reset/$', 'invite.views.reset', name="reset"),
-    url(r'^signup/$', 'invite.views.signup', name="account_signup"),
-    url(r'^about/$', 'invite.views.about', name="about"),
-)
+from invite import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^invite/$', views.invite, name='invite'),
+    url(r'^resend/(?P<code>.*)/$', views.resend, name='resend'),
+    url(r'^revoke/(?P<code>.*)/$', views.revoke, name='revoke'),
+    url(r'^login/$', views.log_in_user, name='login'),
+    url(r'^logout/$', views.log_out_user, name='edit_logout'),
+    url(r'^amnesia/$', views.amnesia, name='amnesia'),
+    url(r'^reset/$', views.reset, name='reset'),
+    url(r'^signup/$', views.signup, name='account_signup'),
+    url(r'^about/$', views.about, name='about'),
+]
