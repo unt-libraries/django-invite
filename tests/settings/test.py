e4b0efe5887621674abb372c67b502dd06ea30eb
Gio Gottardi <somexpert@gmail.com>
2017-10-06 13:03:14 -0500

    Add new options to dev and test settings.

--- tests/settings/test.py
+++ tests/settings/test.py
@@ -1,6 +1,12 @@
 from .common import *  # noqa
 
 
+INVITE_SHOW_EMAILS = True
+
+INVITE_OPEN_INVITE_CUTOFF = 1
+
+INVITE_REGISTRATION_CUTOFF = 1
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
