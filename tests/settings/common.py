e42663ef64e6a1187e6c7dcce5c77efa0b9b7649
Gio Gottardi <somexpert@gmail.com>
2019-05-22 10:58:03 -0500

    Use new middleware setting.

--- tests/settings/common.py
+++ tests/settings/common.py
@@ -21,7 +21,7 @@ INSTALLED_APPS = (
     'invite'
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
