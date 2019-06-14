3981b74f97c388b03c595879b548d7a74029d02a
Gio Gottardi <somexpert@gmail.com>
2019-06-14 10:55:59 -0500

    Use lists in settings.

--- tests/settings/common.py
+++ tests/settings/common.py
@@ -27,7 +27,7 @@ TEMPLATES = [
 
 SITE_ID = 1
 
-INSTALLED_APPS = (
+INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -35,16 +35,16 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'invite'
-)
+]
 
-MIDDLEWARE = (
+MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
+]
 
 ROOT_URLCONF = 'tests.urls'
 
