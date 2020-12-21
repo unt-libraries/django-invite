68d9e47e7590206749358c58dc2dc573a005904b
madhulika95b <srisudhamadhulika.bayyavarapu@unt.edu>
2020-12-21 12:07:02 -0600

    Replace re_path with path.

--- invite/urls.py
+++ invite/urls.py
@@ -1,18 +1,17 @@
-from django.urls import re_path
-
+from django.urls import path
 from invite import views
 
 app_name = 'invite'
 urlpatterns = [
-    re_path(r'^$', views.index, name='index'),
-    re_path(r'^invite/$', views.invite, name='invite'),
-    re_path(r'^resend/(?P<code>.*)/$', views.resend, name='resend'),
-    re_path(r'^revoke/(?P<code>.*)/$', views.revoke, name='revoke'),
-    re_path(r'^login/$', views.log_in_user, name='login'),
-    re_path(r'^logout/$', views.log_out_user, name='edit_logout'),
-    re_path(r'^amnesia/$', views.amnesia, name='amnesia'),
-    re_path(r'^reset/$', views.reset, name='reset'),
-    re_path(r'^signup/$', views.signup, name='account_signup'),
-    re_path(r'^about/$', views.about, name='about'),
-    re_path(r'^check/$', views.check, name='check'),
+    path('', views.index, name='index'),
+    path('invite/', views.invite, name='invite'),
+    path('resend/(<slug:code>)/', views.resend, name='resend'),
+    path('revoke/(<slug:code>)/', views.revoke, name='revoke'),
+    path('login/', views.log_in_user, name='login'),
+    path('logout/', views.log_out_user, name='edit_logout'),
+    path('amnesia/', views.amnesia, name='amnesia'),
+    path('reset/', views.reset, name='reset'),
+    path('signup/', views.signup, name='account_signup'),
+    path('about/', views.about, name='about'),
+    path('check/', views.check, name='check'),
 ]
