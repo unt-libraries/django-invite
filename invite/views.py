a8d69a564030599700a2e97f4391385fa4c54ed5
gracieflores <53153376+gracieflores@users.noreply.github.com>
2022-08-31 16:09:02 -0500

    add try/except to allow/skip forms that don't exist

--- invite/views.py
+++ invite/views.py
@@ -271,38 +271,43 @@ def invite(request):
         invite_item_formset = InviteItemFormSet(request.POST, request.FILES)
         if invite_item_formset.is_valid():
             for form in invite_item_formset.forms:
-                # make invite for each form
-                i = Invitation.objects.create(
-                    first_name=form.cleaned_data['first_name'],
-                    last_name=form.cleaned_data['last_name'],
-                    username=form.cleaned_data['username'],
-                    email=form.cleaned_data['email'],
-                    is_super_user=form.cleaned_data['is_super_user'],
-                )
-                i.custom_msg = (
-                    invite_item_formset
-                    .forms[0]
-                    .cleaned_data['greeting']
-                )
-                # set m2m relationships from initial object creation
-                permissions = (
-                    invite_item_formset
-                    .forms[0]
-                    .cleaned_data['permissions']
-                )
-                groups = (
-                    invite_item_formset
-                    .forms[0]
-                    .cleaned_data['groups']
-                )
+                try:
+                    # make invite for each form
+                    i = Invitation.objects.create(
+                        first_name=form.cleaned_data['first_name'],
+                        last_name=form.cleaned_data['last_name'],
+                        username=form.cleaned_data['username'],
+                        email=form.cleaned_data['email'],
+                        is_super_user=form.cleaned_data['is_super_user'],
+                    )
+                    i.custom_msg = (
+                        invite_item_formset
+                        .forms[0]
+                        .cleaned_data['greeting']
+                    )
+                    # set m2m relationships from initial object creation
+                    permissions = (
+                        invite_item_formset
+                        .forms[0]
+                        .cleaned_data['permissions']
+                    )
+                    groups = (
+                        invite_item_formset
+                        .forms[0]
+                        .cleaned_data['groups']
+                    )
 
-                for permission in permissions:
-                    i.permissions.add(permission)
-                for group in groups:
-                    i.groups.add(group)
-                # send the email invitation
-                i.send(request=request)
-                i.save()
+                    for permission in permissions:
+                        i.permissions.add(permission)
+                    for group in groups:
+                        i.groups.add(group)
+                    # send the email invitation
+                    i.send(request=request)
+                    i.save()
+                except KeyError:
+                    # except KeyError due to possible incorrect form count
+                    # due to user refreshing and re-submitting forms
+                    continue
             return HttpResponseRedirect(reverse('invite:index'))
     else:
         invite_item_formset = InviteItemFormSet()
